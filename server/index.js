69dfe2c78f49dda999138026d70a2c8acd8da284
ritesh-1997 <riteshg763@gmail.com>
2023-05-03 23:47:53 +0530

    React Memory Share Project

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv';
+
+
+const app = express();
+dotenv.config();
+
+
+app.use(bodyParser.json({limit : "30mb", extended : true}));
+app.use(bodyParser.urlencoded({limit : "30mb", extended : true}));
+app.use(cors());
+app.use('/posts',postRoutes); 
+
+//const CONNECTION_URL = "mongodb+srv://riteshgupta:SOnu7172@cluster0.4qhedza.mongodb.net/test"
+const PORT = process.env.PORT || 5001
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=> app.listen(PORT, () => console.log(`Server Running on PORT : ${PORT}`) )) 
+    .catch((error) => console.log(error.message))
+
