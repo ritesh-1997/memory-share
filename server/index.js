083bb5c284b49baf260193f7c4af7b6069cbe0c1
ritesh-1997 <riteshg763@gmail.com>
2023-06-06 21:52:23 +0530

    For getting IP

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import paymentsRoutes from "./routes/payments.js";
 import cartRoutes from "./routes/cart.js";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import { exec } from "child_process";
 
 const app = express();
 dotenv.config();
@@ -57,6 +58,25 @@ app.use("/payments", paymentsRoutes);
 app.use("/v1/cart", cartRoutes);
 
 const PORT = process.env.PORT || 5001;
+
+// Run the dig command
+exec(
+  "dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com",
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error.message}`);
+      return;
+    }
+
+    if (stderr) {
+      console.error(`Command stderr: ${stderr}`);
+      return;
+    }
+
+    // Print the command output
+    console.log(stdout);
+  }
+);
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
